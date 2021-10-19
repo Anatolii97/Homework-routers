e42f0c2d330e83bdc8b5d4f4efdeedc3d3ee3439
Anatolii97 <anatoliizalizkii@gmail.com>
2021-10-19 18:46:02 +0300

    Homework-routers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import './theme/App.css';
+import { Switch, Route } from "react-router-dom";
+import { NotFound } from "./pages/NotFoundPage";
+import { MoviesPage } from "./pages/MoviesPage";
+import { MovieDetails } from "./pages/MovieDetailsPage"
+import { MainMoviesPage } from "./pages/MainMoviesPage"
+import { Link } from "react-router-dom";
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <header>
+            <nav className="navigation">
+            <Link to="/">
+                <Stack className="nav-button" spacing={2} direction="row">
+                    <Button variant="contained">Home page</Button>
+                </Stack>
+            </Link>
+            <Link to="/movies">
+                <Stack className="nav-button" spacing={2} direction="row">
+                    <Button variant="contained">To list of popular films</Button>
+                </Stack>
+            </Link>
+            </nav>
+    </header> 
+    <Switch>
+        <Route exact path="/" component={MainMoviesPage}/>
+        <Route exact path="/movies" component={MoviesPage}/>
+        <Route exact path="/movies/movie:id" component={MovieDetails}/>
+        <Route component={NotFound} />
+    </Switch>
+    <footer>&copy; October 2021</footer> 
+    </>
+  )
 }
 
 export default App;
